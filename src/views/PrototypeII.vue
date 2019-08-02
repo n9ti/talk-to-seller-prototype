af72dbca55283d9094a01a50f5f033d9211cbbc5
Nati <nati.n@linecorp.com>
2019-08-02 14:57:02 +0700

    III

--- src/views/PrototypeII.vue
+++ src/views/PrototypeII.vue
@@ -1,7 +1,52 @@
 <template>
-  <div></div>
+  <div class="min-h-screen">
+    <section class="">
+      <div class="container mx-auto">
+        <div class="px-6 py-4">
+          <img src="../assets/products/adidas.jpg" alt="" />
+
+          <div class="mt-4 flex flex-col ml-4">
+            <p class="break-words font-bold text-gray-800">
+              NEW BALANCE M997NM MADE IN USA
+            </p>
+            <span class="text-gray-600">3,200.-</span>
+          </div>
+
+          <div class="mt-4 flex items-center">
+            <button
+              @click="
+                sendMessages(
+                  'https://shopee.co.th/Ready-Stock-NEW-BALANCE-M997NM-MADE-IN-USA1849378307-i.162968657.2495003037'
+                )
+              "
+              class="mx-2 w-full bg-green-400 text-xl text-white py-2 px-4 hover:bg-green-500 rounded"
+            >
+              Talk to seller
+            </button>
+            <button
+              class="mx-2 w-full bg-green-400 text-xl text-white py-2 px-4 hover:bg-green-500 rounded"
+            >
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    window.liff.init();
+  },
+  methods: {
+    async sendMessages(msg) {
+      window.liff.openWindow({
+        url: `line://oaMessage/@434xepgn/?${msg}`,
+        external: true
+      });
+    }
+  }
+};
 </script>
--- src/views/PrototypeIII.vue
+++ src/views/PrototypeIII.vue
@@ -1,7 +1,118 @@
 <template>
-  <div></div>
+  <div class="min-h-screen">
+    <section class="">
+      <div class="container mx-auto">
+        <div class="px-6 py-4">
+          <img src="../assets/products/adidas.jpg" alt="" />
+          <div class="mt-4 flex flex-col ml-4">
+            <p class="break-words font-bold text-gray-800">
+              NEW BALANCE M997NM MADE IN USA
+            </p>
+            <span class="text-gray-600">3,200.-</span>
+          </div>
+
+          <div class="mt-4 flex items-center">
+            <button
+              @click="openModel()"
+              class="mx-2 w-full bg-green-400 text-xl text-white py-2 px-4 hover:bg-green-500 rounded"
+            >
+              Talk to seller
+            </button>
+            <button
+              class="mx-2 w-full bg-green-400 text-xl text-white py-2 px-4 hover:bg-green-500 rounded"
+            >
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--  -->
+    <div v-show="show">
+      <div
+        @click="show = false"
+        class="fixed top-0 left-0 right-0 bottom-0 bg-black opacity-50"
+      ></div>
+      <div class="fixed left-0 right-0 bottom-0">
+        <div class="m-2 border bg-white">
+          <div class="flex items-center border-b p-3">
+            <img
+              src="../assets/products/adidas.jpg"
+              class="h-12 w-12 bg-gray-500"
+            />
+            <div class="flex flex-col ml-4">
+              <p class="break-words font-bold text-gray-800">
+                NEW BALANCE M997NM MADE IN USA
+              </p>
+              <span class="text-gray-600">3,200.-</span>
+            </div>
+          </div>
+          <!-- <div class="flex items-center m-2">
+            <div class="flex-auto text-center border-r">ยื่นข้อเสนอ</div>
+            <div class="flex-auto text-center">ซื้อตอนนี้</div>
+            </div> -->
+          <div class="flex w-full bg-gray-200 p-3">
+            <input
+              v-model="msg"
+              type="text"
+              class="flex-auto rounded py-2 px-3 mr-3"
+              placeholder="Aa"
+              ref="textInput"
+            />
+            <button
+              @click="sendMessages()"
+              class="rounded inline-flex items-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentcolor"
+                  d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      show: false,
+      msg: ""
+    };
+  },
+  mounted() {
+    window.liff.init();
+  },
+  methods: {
+    openModel() {
+      this.show = true;
+      this.$nextTick(() => this.$refs.textInput.focus());
+    },
+    async sendMessages() {
+      let url =
+        "https://shopee.co.th/Ready-Stock-NEW-BALANCE-M997NM-MADE-IN-USA1849378307-i.162968657.2495003037";
+      try {
+        await window.liff.sendMessages([
+          {
+            type: "text",
+            text: `${this.msg}\r\n${url}`
+          }
+        ]);
+      } finally {
+        this.show = false;
+        this.$toast.top("Messages sent");
+      }
+    }
+  }
+};
 </script>
